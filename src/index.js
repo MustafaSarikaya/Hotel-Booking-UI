1293855bea072830140d8697bf11187f0666e978
MustafaSarikaya <msarikay@uottawa.ca>
2023-07-24 00:27:51 -0400

    browserRoute basename

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ i18n
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
